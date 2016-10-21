536156dedab3f3b7a8b0a75de2f11636997f0008
Tao Mo <taomo@TaodeMacBook-Pro.local>
2016-10-21 18:59:12 -0500

    test1

--- src/leetcoder/no_1.java
+++ src/leetcoder/no_1.java
@@ -3,20 +3,7 @@ package leetcoder;
 import java.util.HashMap;
 
 public class no_1 {
-	public static int[] twoSum(int[] nums, int target) {
-		HashMap<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < nums.length; i++) {
-			if (map.containsKey(nums[i])) return new int[] {map.get(nums[i]),i};
-			map.put(target-nums[i], i);
-		}
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		int[] inp = {1,3,7,9}; 
-		System.out.println(twoSum(inp,16)[0]+","+twoSum(inp,16)[1]);
 
-	}
 }
 
 //Old
