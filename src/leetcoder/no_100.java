197eb2ad284ef0a2e7a762bf3b0006138037a541
Tao Mo <taomo@TaodeMacBook-Pro.local>
2016-10-21 19:02:31 -0500

    test2

--- src/leetcoder/no_100.java
+++ src/leetcoder/no_100.java
@@ -3,16 +3,7 @@ package leetcoder;
 public class no_100 {
 	
 	public class TreeNode {
-		      int val;
-		      TreeNode left;
-		      TreeNode right;
-		     TreeNode(int x) { val = x; }
-		  }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-    	if (p==null&&q==null) return true;
-    	if (p==null||q==null) return false;
-    	return (p.val == q.val) &&  isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    }
+	 }
     
 }
 
